7a11350eb999612e6f6bea1bad2ba24aac963107
Shriyans Gandhi <shri30yans@gmail.com>
2022-01-21 11:36:04 +0530

    Updates

--- popup.js
+++ popup.js
@@ -1,21 +1,11 @@
-let button = document.getElementById("button");
-
-function main(value) {
-    // Value is the text got from the input field in the popup
-    ASIN = document.getElementById("ASIN")
-    ASIN.setAttribute("value", value)
-    document.getElementById('add-to-wishlist-button-submit').click()
-    console.log("ASIN value changed.")
-};
-
-
+// popup.js
 document.addEventListener('DOMContentLoaded', documentEvents, false);
 
 function documentEvents() {
+    let button = document.getElementById("button");
     button.addEventListener('click', async () => {
         let value = document.getElementById('userinput').value;
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        
         chrome.scripting.executeScript({
             target: {
                 tabId: tab.id
@@ -24,8 +14,21 @@ function documentEvents() {
             args: [value]
         })
     });
-    
 }
+
+function main(value) {
+    // value is the text got from the input field in the popup
+    ASIN = document.getElementById("ASIN")
+    if (ASIN !== null) {
+        ASIN.setAttribute("value", value)
+        document.getElementById('add-to-wishlist-button-submit').click()
+        console.log("ASIN value changed.")
+    } else {
+        alert("This is not a valid Amazon page.\nYou can only run this extension in any Amazon product page.")
+    }
+
+};
+
 
 
 
